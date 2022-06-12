abafdbdec4e9e67854c6fcc43cb5ca02658d4bd5
Andre Conjo <andreconjo@iMac-de-Andre.local>
2022-06-12 14:23:29 -0300

    Adicionado mascara de input

--- src/app/pages/termos/termos.component.ts
+++ src/app/pages/termos/termos.component.ts
@@ -10,7 +10,11 @@ import { Location } from '@angular/common';
 export class TermosComponent implements OnInit {
   termsAccepted: boolean = false;
 
-  constructor(private router: Router, private _location: Location) {}
+  phoneNumber: any;
+
+  constructor(private router: Router, private _location: Location) {
+
+  }
 
   ngOnInit(): void {}
 
@@ -24,5 +28,24 @@ export class TermosComponent implements OnInit {
 
   voltarPagina() {
     this._location.back();
+  }
+
+  onInputChange(event: any) {
+    console.log('iameter')
+    let newVal = event.replace(/\D/g, '');
+
+    if (newVal.length === 0) {
+      newVal = '';
+    } else if (newVal.length <= 3) {
+      newVal = newVal.replace(/^(\d{0,2})/, '($1)');
+    } else if (newVal.length <= 6) {
+      newVal = newVal.replace(/^(\d{0,2})(\d{0,1})(\d{0,4})/, '($1) $2 $3');
+    } else if (newVal.length <= 10) {
+      newVal = newVal.replace(/^(\d{0,2})(\d{0,1})(\d{0,4})(\d{0,4})/, '($1) $2 $3-$4');
+    } else {
+      newVal = newVal.substring(0, 10);
+      newVal = newVal.replace(/^(\d{0,2})(\d{0,1})(\d{0,4})(\d{0,4})/, '($1) $2 $3-$4');
+    }
+    this.phoneNumber = newVal;
   }
 }
